b9fd337c1324022a6b3d8ab393914c156407d7c2
Lindelwa <lindelwa.codes@gmail.com>
2023-08-28 09:07:30 +0200

    Change color of the box on mouse over

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+const createDiv = (...classList) => {
+  const div = document.createElement("div");
+  div.classList = [...classList].join(" ");
+  return div;
+};
+
+const createGrid = (rows, columns) => {
+  const container = document.querySelector(".container");
+
+  for (let i = 0; i < rows; i++) {
+    let row = createDiv("row");
+
+    for (let j = 0; j < columns; j++) {
+      const column = createDiv("column");
+      row.appendChild(column);
+    }
+
+    container.appendChild(row);
+  }
+};
+
+createGrid(16, 16);
+
+const boxes = document.querySelectorAll(".column");
+
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", (e) => {
+    box.style.backgroundColor = "yellow";
+  });
+});
